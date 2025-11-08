98a3ae6e2323b39c9894986ba779a685f25ca22d
Taufik Amaryansyah <taufik.amary@gmail.com>
2025-11-08 16:09:41 +0700

    Migrate `AndroidManifest.xml`, resource files, and Bazel rules to new directory structure under `src/main`. Update `install.sh` to reflect changes. Remove redundant `LibraryManifest.xml`.

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 set -e
 
 echo "Building app..."
-bazel build //app/src/main:app
+bazel build //app:app
 
 echo "Installing app..."
-adb install -r bazel-bin/app/src/main/app.apk
+adb install -r bazel-bin/app/app.apk
 
 echo "Starting app..."
 adb shell am start -n com.amary.my.music/.MainActivity
