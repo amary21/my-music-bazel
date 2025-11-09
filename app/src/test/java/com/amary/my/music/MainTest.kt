57dd342aebeb297749104d31d0199e7267ff3f3b
Taufik Amaryansyah <taufik.amary@gmail.com>
2025-11-09 18:45:41 +0700

    Add unit testing setup:
    - Introduced `ExampleUnitTest` for local unit testing.
    - Added `MainTest` class for test suite organization.
    - Configured Bazel for running tests using `kt_jvm_test`.
    - Included necessary testing dependencies (`JUnit`, `MockK`, Kotlin Coroutines Test) in `MODULE.bazel` and `maven_install.json`.

---
+++ app/src/test/java/com/amary/my/music/MainTest.kt
@@ -0,0 +1,11 @@
+package com.amary.my.music
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    ExampleUnitTest::class,
+    // Add other test classes here
+)
+class MainTest
